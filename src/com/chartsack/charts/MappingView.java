cea88702e2445b44db785b5a8cc59d9d3849912f
apps4av <apps4av@gmail.com>
2014-11-18 15:15:08 -0500

    added self made animation as alpha animation is slow

--- src/com/chartsack/charts/MappingView.java
+++ src/com/chartsack/charts/MappingView.java
@@ -1,3 +1,15 @@
+/*
+Copyright (c) 2012, Apps4Av Inc. (apps4av.com) 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    *     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    *
+    *     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, LONGITUDE, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package com.chartsack.charts;
 
 import com.chartsack.charts.touch.MultiTouchController;
@@ -10,10 +22,11 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
 import android.widget.ImageButton;
 import android.widget.ZoomControls;
 
@@ -33,7 +46,6 @@ public class MappingView extends View implements MultiTouchObjectCanvas<Object> 
     private StorageService               mService;
     private ZoomControls 				 mZoomControls;
     private ImageButton 				 mTopButton;
-    private AlphaAnimation               mZoomAnimation;
 
     /**
      * 
@@ -74,32 +86,52 @@ public class MappingView extends View implements MultiTouchObjectCanvas<Object> 
         setBackgroundColor(Color.BLACK);
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
-        /*
-         * Zoom button animation, hide time of 5 seconds, and 2 second animation
-         */
-        mZoomAnimation = new AlphaAnimation(2.0f, 0.0f);
-        mZoomAnimation.setDuration(1000);
-        mZoomAnimation.setStartOffset(5000);
-        mZoomAnimation.setFillAfter(true);
     }
 
 
+    /**
+     * 
+     */
+    private void startAnimation(boolean start) {
+        if(null == mZoomControls) {
+        	return;
+        }
+    	mZoomControls.setVisibility(View.VISIBLE);
+    	// Show if animation is started
+        if(start) {
+        	return;
+        }
+        // Hide zoom controls after 5 seconds
+        mHandler.removeMessages(0, null);
+        mHandler.sendEmptyMessageDelayed(0, 5000);
+    }
+    
+    /**
+     * 
+     */
+    Handler mHandler = new Handler() {
+    	@Override
+   	    public void handleMessage(Message msg) {
+            // Hide after timeout if animation stopped
+            mZoomControls.setVisibility(View.INVISIBLE);    		
+    	}
+    };
+    	
     /**
      * Add zoom control for all map views
      */
     public void setZoomControls(ZoomControls zc) {
     	mZoomControls = zc;
-        mZoomControls.setAnimation(mZoomAnimation);
         mZoomControls.setOnZoomInClickListener(new OnClickListener() {
 			public void onClick(View v) {
-	        	mZoomControls.startAnimation(mZoomAnimation);
+	        	startAnimation(false);
 				getService().getScale().zoomIn();
 				getService().loadBitmap(null);
 			}
 		});
         mZoomControls.setOnZoomOutClickListener(new OnClickListener() {
 			public void onClick(View v) {
-	        	mZoomControls.startAnimation(mZoomAnimation);
+	        	startAnimation(false);
 				getService().getScale().zoomOut();
 				getService().loadBitmap(null);
 			}
@@ -142,22 +174,14 @@ public class MappingView extends View implements MultiTouchObjectCanvas<Object> 
 	        /*
 	         * Show the zoom button
 	         */
-	        if(null != mZoomControls) {
-	        	mZoomControls.setAlpha(1.0f);
-	        }
+	        startAnimation(true);
 	    }
 	    else if(e.getAction() == MotionEvent.ACTION_UP) {
 	    	/*
 	    	 * Decode region of the image when we lift finger
 	    	 */
-	    	
+	        startAnimation(false);	    	
 	      	mService.loadBitmap(null);
-	        if(null != mZoomControls) {
-	        	/*
-	        	 * Start to hide zoom control
-	        	 */
-	        	mZoomControls.startAnimation(mZoomAnimation);
-	        }
 	    }
 	    /*
 	     * This slows downs panning when zoomed in
