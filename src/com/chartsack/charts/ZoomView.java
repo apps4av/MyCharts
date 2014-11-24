11f5f54b9cb3dd81c2cee35dad00519565bedd04
apps4av <apps4av@gmail.com>
2014-11-24 11:04:41 -0500

    cosmetic

--- src/com/chartsack/charts/ZoomView.java
+++ src/com/chartsack/charts/ZoomView.java
@@ -18,7 +18,6 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
